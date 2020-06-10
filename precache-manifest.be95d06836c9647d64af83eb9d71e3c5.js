d396ecbe39894d428c93a92c2fce2df6da61fcbf
virgoaugustine <virgoaugustine@gmail.com>
2020-06-10 12:37:23 +0000

    Updates

---
+++ precache-manifest.be95d06836c9647d64af83eb9d71e3c5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97eeecf12b3b395b99d79f8db26fee3a",
+    "url": "/text-similarity-frontend/index.html"
+  },
+  {
+    "revision": "f19daebe7f0b6765e390",
+    "url": "/text-similarity-frontend/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "ff536c3fb4257f337869",
+    "url": "/text-similarity-frontend/static/css/main.3d4ed0c6.chunk.css"
+  },
+  {
+    "revision": "f19daebe7f0b6765e390",
+    "url": "/text-similarity-frontend/static/js/2.5e52811b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/text-similarity-frontend/static/js/2.5e52811b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff536c3fb4257f337869",
+    "url": "/text-similarity-frontend/static/js/main.53aa7d62.chunk.js"
+  },
+  {
+    "revision": "d2eb9b486927b98771d0",
+    "url": "/text-similarity-frontend/static/js/runtime-main.4f2aa8d6.js"
+  }
+]);
